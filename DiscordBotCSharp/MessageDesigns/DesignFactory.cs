3989dc3a5f29d437cd2bf0b6e7ab6d1ba87cefb5
timo schützler <timoschuetzler@gmx.de>
2022-01-24 13:29:22 +0100

    Weiteren Text ausgelagert

--- DiscordBotCSharp/MessageDesigns/DesignFactory.cs
+++ DiscordBotCSharp/MessageDesigns/DesignFactory.cs
@@ -7,20 +7,7 @@ using System.Text;
 namespace DiscordBotCSharp.MessageDesigns
 {
     public class DesignFactory
-    {
-        private const string DISCORDLIB = "https://docs.stillu.cc/api/Discord.EmbedFieldBuilder.html";
-        private const string EVERYONE_TAG = "@everyone";
-        private const string WOLFI_BOT_PICUTRE = "https://i.imgur.com/3h5CSAf.png";
-        private const string EMBED_FOOTER = "Made by Wolfi";
-        private const string WOLFI_IMAGE = "https://cdn.discordapp.com/avatars/323381699675422724/be60edbf146f24c975d708e05d2030b1.png?size=1024";
-        private const string WOLFIS_NAME = "Wolfi";
-        private const string BOT_CREATED_TIME = "03.12.2020 19:22:59";
-        private const string BOT_UPDATE_TIME = "24.01.2022 12:09:55";
-        private const string COMMANDS = ";MyUserInfo\n;Info\n;TTT\n";
-        private const string PATCH_NOTES = "Bot Language changed to english";
-        private const string PROJECT_LINK = "https://github.com/Wolfi1802/DiscordBotManager";
-
-
+    { 
         public DiscordEmbedBuilder GetEmbed((string name, string value, bool inline) field,
             string Title = "", string Description = "")
         {
@@ -34,18 +21,18 @@ namespace DiscordBotCSharp.MessageDesigns
             List<(string name, string value, bool inline)> fieldList = null)
         {
             var embed = new DiscordEmbedBuilder { Footer = new DiscordEmbedBuilder.EmbedFooter() };
-            embed.WithAuthor(name: WOLFIS_NAME, url: WOLFI_IMAGE, iconUrl: WOLFI_IMAGE);
+            embed.WithAuthor(name: TextFragments.WOLFIS_NAME, url: TextFragments.WOLFI_IMAGE, iconUrl: TextFragments.WOLFI_IMAGE);
             embed.Color = DiscordColor.CornflowerBlue;
-            embed.Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail { Url = WOLFI_BOT_PICUTRE };
+            embed.Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail { Url = TextFragments.WOLFI_BOT_PICUTRE };
             embed.Timestamp = DateTimeOffset.Now;
-            embed.Footer.IconUrl = WOLFI_BOT_PICUTRE;
-            embed.Footer.Text = "WolfiBot Informations";
+            embed.Footer.IconUrl = TextFragments.WOLFI_BOT_PICUTRE;
+            embed.Footer.Text = TextFragments.BOT_INFO;
 
             if (!string.IsNullOrEmpty(Title))
-                embed.Title = $"Informationen to Bot";
+                embed.Title = TextFragments.BOT_INFO;
 
             if (!string.IsNullOrEmpty(Title))
-                embed.Description = "This Bot is a free open Source Project,type ;help for more informations about commands";
+                embed.Description = TextFragments.BOT_DESC;
 
             if (fieldList != null && fieldList.Count != 0)
             {
--- DiscordBotCSharp/MessageDesigns/TextFragments.cs
+++ DiscordBotCSharp/MessageDesigns/TextFragments.cs
@@ -16,23 +16,33 @@ namespace DiscordBotCSharp.MessageDesigns
         public const string EMBED_FOOTER = "Made by Wolfi";
         public const string WOLFI_IMAGE = "https://cdn.discordapp.com/avatars/323381699675422724/be60edbf146f24c975d708e05d2030b1.png?size=1024";
         public const string BOT_CREATED_TIME = "03.12.2020 19:22:59";
-        public const string BOT_UPDATE_TIME = "24.01.2022 12:09:55";
-        public const string COMMANDS = ";MyUserInfo\n;Info\n;TTT\n";
+        public const string BOT_UPDATE_TIME = "24.01.2022 13:23:55";
+        public const string COMMANDS = ";myuserinfo\n;info\n;ttt\n;getgit";
         public const string PATCH_NOTES = "Bot Language changed to english";
         public const string PROJECT_LINK = "https://github.com/Wolfi1802/DiscordBotManager";
+        public const string WOLFIS_NAME = "Wolfi";
 
         public const string YOUR_ID = "Your Id on Discord: ";
         public const string PERSONAL_INFOS = "Your Personal informations";
-        public const string SERVER_JOIN = "You Join this Server on";
+        public const string SERVER_JOIN = "You Join this Server on ";
         public const string LIST_COMMANDS = "List of current Commands";
-        public const string CREATED  = "Created";
+        public const string CREATED = "Created";
         public const string NOBODY = "--Nobody--";
         public const string HELPER = "Helper";
         public const string LAST_UPDATE = "Last Update";
         public const string UPDATE_NOTES = "Update Notes";
-        public const string BOT_INFO = "Informationen to Bot";
+        public const string BOT_INFO = "Informations to Bot";
         public const string BOT_DESC = "This Bot is a free open Source Project,type ;help for more informations about commands";
         public const string PROJECT_NAME = "Project Link: ";
+
+        #region SECRET WOLFI TEXT
+        public const string DELETE_ERROR = "Error while delete";
+        public const string AVIABLE_DELETE_MESS = "You can only delete";
+        public const string RIP_DELETE = "Your cant delete Messages older than 13 days.";
+        public const string SUCCESSFUL = "Successful";
+        public const string DELETED = "deleted";
+        public const string MESSAGE = "Messages";
+        #endregion
 
 
     }
